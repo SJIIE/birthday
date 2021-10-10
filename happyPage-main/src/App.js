c7adacface16d9934f5a5afab7ff8e91564ddd93
shuaimengjie <s.my188@163.com>
2021-10-10 22:10:50 +0800

    latest

--- happyPage-main/src/App.js
+++ happyPage-main/src/App.js
@@ -15,24 +15,28 @@ const onSearch = value => {
 function App() {
   const [data, setData] = useState([]);
 
-  const santinizeName = (text) => {
-    var test_txt = ["世间你是最美丽", "你是最美丽的风景", "我爱世间的一切，尤其是你", "今早天气很好，你也很美", "就算有天没音乐，我只见见你也好", "你出现在我诗的每一页", "就算大雨让整座城市颠倒，我会给你怀抱", "我的心里只有你没有她"];
+  const santinizeName = (test_txt) => {
     var index = Math.ceil(Math.random() * test_txt.length) - 1
     //return text.replace(/你/, "李柯淇");
     return test_txt[index].replace(/你/, "李柯淇");
     //  return text.replace(/你/, "李柯淇");
   }
 
-  useEffect(() => {
-        let loop =1;
-        const requestPool = [];
-        while (loop--){
-            requestPool.push(axios.get(
-                'api1/api.php',
-            ))
-        }
-        axios.all(requestPool).then(axios.spread((...arg) => setData(arg.map(e=>santinizeName(e.data)))))
-  },[]); 
+  useEffect(  () => {
+  //       let loop =1;
+  //       const requestPool = [];
+  //       while (loop--){
+  //           requestPool.push(axios.get(
+  //               'api1/api.php',
+  //           ))
+  //       }
+  //       axios.all(requestPool).then(axios.spread((...arg) => setData(arg.map(e=>santinizeName(e.data)))))
+  var test_txt = ["世间你是最美丽", "你是最美丽的风景", "我爱世间的一切，尤其是你", "今早天气很好，你也很美", "就算有天没音乐，我只见见你也好", "你出现在我诗的每一页", "就算大雨让整座城市颠倒，我会给你怀抱", "我的心里只有你没有她"];
+  var index = Math.ceil(Math.random() * test_txt.length) - 1
+  setData([test_txt[index].replace(/你/, "李柯淇")])
+  },[]
+
+  ); 
 
   return (
     <div className="App">
